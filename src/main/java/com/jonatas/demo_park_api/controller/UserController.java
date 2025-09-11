c096b83d406c314dc63ac96de3cc55232abb3f15
Jonatas Freire <jonatasf1997@hotmail.com>
2025-09-11 10:03:22 -0300

    Implementa o método de criação de usuário no UserController e adiciona a anotação @Transactional no UserService

--- src/main/java/com/jonatas/demo_park_api/controller/UserController.java
+++ src/main/java/com/jonatas/demo_park_api/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.jonatas.demo_park_api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jonatas.demo_park_api.entity.User;
 import com.jonatas.demo_park_api.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +19,9 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User newUser = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
 }
--- src/main/java/com/jonatas/demo_park_api/service/UserService.java
+++ src/main/java/com/jonatas/demo_park_api/service/UserService.java
@@ -1,7 +1,9 @@
 package com.jonatas.demo_park_api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jonatas.demo_park_api.entity.User;
 import com.jonatas.demo_park_api.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +13,10 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 
 }
